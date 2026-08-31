c1ff756b0414800c9543bebfca822fb81f10b2f2
Webboomx00 <kevinadrians00@gmail.com>
2026-08-31 16:23:23 +0700

    v2 pda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.kevin.wmsscanner"
         minSdk = 26
         targetSdk = 37
-        versionCode = 18 // must be strictly higher than last time
-        versionName = "2.10"
+        versionCode = 19 // must be strictly higher than last time
+        versionName = "2.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
